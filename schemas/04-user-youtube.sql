ead9d52b08516e0dde184508eccbdb8c047b9812
Philipp Pfeiffenberger <git@notphil.com>
2010-05-12 00:54:27 -0700

    Added schema for user youtube relations

---
+++ schemas/04-user-youtube.sql
@@ -0,0 +1,37 @@
+DROP TABLE IF EXISTS `youtube_videos`;
+SET @saved_cs_client     = @@character_set_client;
+SET character_set_client = utf8;
+CREATE TABLE `youtube_videos` (
+  `youtube_id` varchar(16) default NULL,
+  `title` varchar(60) default NULL,
+  `artist` varchar(60) default NULL,
+  `description` varchar(255) default NULL,
+  `view_count` int(10) unsigned default NULL,
+  `duration` int(8) unsigned default NULL,
+  `saved_at` timestamp NOT NULL default CURRENT_TIMESTAMP,
+  `thumbnail_1` varchar(255) default NULL,
+  `thumbnail_2` varchar(255) default NULL,
+  `thumbnail_3` varchar(255) default NULL,
+  `flash_url` varchar(255) default NULL,
+  PRIMARY KEY  (`youtube_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+SET character_set_client = @saved_cs_client;
+
+--
+-- Table structure for table `user_youtube_map`
+--
+DROP TABLE IF EXISTS `user_youtube_map`;
+SET @saved_cs_client     = @@character_set_client;
+SET character_set_client = utf8;
+CREATE TABLE `user_youtube_map` (
+  `id` int(64) unsigned NOT NULL auto_increment,
+  `user_id` int(64) NOT NULL,
+  `youtube_id` varchar(16) NOT NULL,
+  `created_at` timestamp NOT NULL default CURRENT_TIMESTAMP,
+  `next_id` int(64) unsigned default NULL,
+  PRIMARY KEY (`id`),
+  INDEX us (user_id),
+  INDEX yt (youtube_id),
+  UNIQUE KEY us_yt (user_id, youtube_id)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+SET character_set_client = @saved_cs_client;
