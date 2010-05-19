07a4f2c7f649d559d6148ee39ae1f9c1a766674d
Philipp Pfeiffenberger <git@notphil.com>
2010-05-18 22:19:29 -0700

    updated playlist schema

--- schemas/04-user-youtube.sql
+++ schemas/04-user-youtube.sql
@@ -18,20 +18,36 @@ CREATE TABLE `youtube_videos` (
 SET character_set_client = @saved_cs_client;
 
 --
--- Table structure for table `user_youtube_map`
+-- Table structure for table `playlist_youtube_map`
 --
-DROP TABLE IF EXISTS `user_youtube_map`;
+DROP TABLE IF EXISTS `playlist_youtube_map`;
 SET @saved_cs_client     = @@character_set_client;
 SET character_set_client = utf8;
 CREATE TABLE `user_youtube_map` (
   `id` int(64) unsigned NOT NULL auto_increment,
-  `user_id` int(64) NOT NULL,
+  `playlist_id` int(64) NOT NULL,
   `youtube_id` varchar(16) NOT NULL,
   `created_at` timestamp NOT NULL default CURRENT_TIMESTAMP,
   `next_id` int(64) unsigned default NULL,
   PRIMARY KEY (`id`),
-  INDEX us (user_id),
+  INDEX pl (playlist_id),
   INDEX yt (youtube_id),
-  UNIQUE KEY us_yt (user_id, youtube_id)
+  UNIQUE KEY pl_yt (playlist_id, youtube_id)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+SET character_set_client = @saved_cs_client;
+
+--
+-- Table structure for table `user_playlist_map`
+--
+DROP TABLE IF EXISTS `user_playlist_map`;
+SET @saved_cs_client     = @@character_set_client;
+SET character_set_client = utf8;
+CREATE TABLE `user_playlist_map` (
+  `id` int(64) unsigned NOT NULL auto_increment,
+  `user_id` int(64) NOT NULL,
+  `playlist_id` varchar(16) NOT NULL,
+  `created_at` timestamp NOT NULL default CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  INDEX us (user_id)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 SET character_set_client = @saved_cs_client;
