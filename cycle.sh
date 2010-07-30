cfacda6db0e87da13ef96104ca7216185783bdd5
philippp <git@notphi.com>
2010-07-29 17:26:06 -0700

    Cycle now restarts gogreen

--- cycle.sh
+++ cycle.sh
@@ -1,1 +1,1 @@
-export PYTHONPATH="$PYTHONPATH:/var/www/youvj.com:."; kill `cat twistd.pid`; twistd -y server.py
+export PYTHONPATH="$PYTHONPATH:/var/www/youvj.com:."; ./server_gogreen.py -d restart
